6f65786e2243b192e9db762bef3e7111e58f1b9b
Diya <dddd>
2021-11-24 14:12:43 -0600

    hello api

---
+++ routes/routes.js
@@ -0,0 +1,10 @@
+let express=require('express')
+//const {json} = require("express");
+let router=express.Router()
+
+router.get('/',function (req,res,next){
+
+  res.json({'message':'Hello ITEC 2560 web programmers'})
+})
+
+module.exports=router
